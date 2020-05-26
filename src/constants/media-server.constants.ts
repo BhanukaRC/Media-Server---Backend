c536ee43f9cba26e6d64aaf6ae2cc764e82311d8
Bhanuka <bhanuka@yaalalabs.com>
2020-05-27 04:16:35 +0530

    mior fix

--- src/constants/media-server.constants.ts
+++ src/constants/media-server.constants.ts
@@ -1,9 +1,8 @@
-import multer from 'multer';
 
 export const PORT = 9001;
-export const MONGO_URL = "mongodb://localhost:27017/MediaServer";
+export const MONGO_URL = "mongodb://db:27017/MediaServer";
 export const FILE_TYPES = [ "Audio", "Image", "Video", "Other"];
 export const SALT_LIMIT = 11;
 export const PERMISSION_LEVEL = '0744';
-export const ROOT_FOLDER = 'RootFolder';
+export const ROOT_FOLDER = './RootFolder';
 
